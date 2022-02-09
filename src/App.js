7c296b9250dac6a1b413df9abb2a952d914476a6
Noura Magdy <magdynoura095@gmail.com>
2022-02-09 10:57:07 +0200

    cartCommit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CartCounter from './pages/cartCounter';
+// style 
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+        <CartCounter />
+    </div>
+  );
+}
+
+export default App;
